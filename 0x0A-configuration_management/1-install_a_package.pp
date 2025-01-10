bd834c9562203ec421a3b8ad3bbcb05ebc878960
Anas2018EMI <anassaatef@gmail.com>
2025-01-10 03:27:43 -0800

    Initial commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# 1-install_a_package.pp
-
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => 'flask --version | grep -q "Flask 2.1.0"',
-  require => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure => installed,
+# install flask web framework
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
